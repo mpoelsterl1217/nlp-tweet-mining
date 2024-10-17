a2e7f19d49b1c2591f42471a2f4006c2dd8182c7
Mattie Poelsterl <mpoelsterl1217@gmail.com>
2024-10-17 15:40:54 -0500

    Initializers for Tweet and User

--- tweet.py
+++ tweet.py
@@ -1,11 +1,20 @@
-import user.py
-import datetime.py
+import user
+import datetime
+import preprocess
 
 class Tweet:
-    text: str
-    user: User
+    clean_text: str
+    user: user.User
     id: int
     timestamp: datetime.datetime
+
+    ## TODO: add in fields for QTs, RTs, @s
+
+    def __init__(self, raw_text, user, id, timestamp):
+        self.clean_text = preprocess.clean_text(raw_text)
+        self.user = user
+        self.id = id
+        self.timestamp = preprocess.timestamp_to_datetime(timestamp)
 
 
 
--- user.py
+++ user.py
@@ -4,4 +4,6 @@ class User:
     screen_name: str
     id: int
 
-    
+    def __init__(self, screen_name, id):
+        self.screen_name = screen_name
+        self.id = id
