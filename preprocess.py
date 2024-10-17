7e1a49f2032622b097b9c79d0f0f515c3ad6c216
Mattie Poelsterl <mpoelsterl1217@gmail.com>
2024-10-17 15:16:20 -0500

    Created simple preprocessing utilities

---
+++ preprocess.py
@@ -0,0 +1,39 @@
+from ftfy import fix_text
+import unidecode
+from nltk.stem import WordNetLemmatizer as wnl
+
+## Clean up HTML special characters
+def replace_html_chars(text: str) -> str:
+    return fix_text(text)
+
+
+## Replace non-ascii character with ascii
+def replace_non_ascii(text: str) -> str:
+    return unidecode(text)
+
+## TODO: What to do with URLs?
+
+## Clean up whitespace
+def remove_whitespace(text: str) -> str:
+    return " ".join(text.split())
+
+## Lemmatize text
+def lemmatize(text: str) -> str:
+    return wnl().lemmatize(text)
+
+## TODO: Exclude non-English text
+
+
+## TODO: convert hashtags and usernames to natural text
+
+
+## read timestamps into a DateTime object
+def timestamp_to_datetime(timestamp_ms: int):
+    return datetime.datetime.fromtimestamp(timestamp_ms/1000.0)
+
+
+## TODO: subset out dataset
+    ## - No RT or QT
+    ## - RT, no QT
+    ## - QT, no RT
+    ## - QT and RT
