6587e9e57d3083e46676c171f23d2c0fe613970e
Catherine Tawadros <ctawadros21@gmail.com>
2024-10-30 21:09:16 -0500

    find award initial algo

---
+++ clean_dict.py
@@ -0,0 +1,9 @@
+# function to clean dictionary of winners/nominees/awards so that weird things are removed (e.g. ceremony name, tweet links)
+
+def clean_dict(dict):
+    # put imdb aggregation stuff here
+
+    # TODO: parameterize this?
+    if "golden globes"
+
+    # get rid of links (http)
